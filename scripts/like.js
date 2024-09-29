e294af67bb8c7d4b557852e49a5440a2ee0177bb
yannyxcode <babyzuzi@yandex.ru>
2024-09-29 22:58:13 +0300

    29.09.2024

--- scripts/like.js
+++ scripts/like.js
@@ -8,34 +8,35 @@
 Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
-const likeHeartArray = document.querySelectorAll('.like-icon');
-const likeButtonArray = document.querySelectorAll('.card__like-button');
-const iconButtonArray = document.querySelectorAll('.card__icon-button');
+const likeHeartArray = document.querySelectorAll(".like-icon");
+const likeButtonArray = document.querySelectorAll(".card__like-button");
+const iconButtonArray = document.querySelectorAll(".card__icon-button");
 
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
-    toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+    iconButton.onclick = () =>
+        toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
 });
 
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+    button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
 });
 
 function toggleIsLiked(heart, button) {
-  heart.classList.toggle('is-liked');
-  setButtonText(heart, button);
+    heart.classList.toggle("is-liked");
+    setButtonText(heart, button);
 }
 
 function setButtonText(heart, button) {
-  if ([...heart.classList].includes('is-liked')) {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Unlike'),
-      500
-    );
-  } else {
-    setTimeout(
-      () => (button.querySelector('.button__text').textContent = 'Like'),
-      500
-    );
-  }
+    if ([...heart.classList].includes("is-liked")) {
+        setTimeout(
+            () =>
+                (button.querySelector(".button__text").textContent = "Unlike"),
+            500
+        );
+    } else {
+        setTimeout(
+            () => (button.querySelector(".button__text").textContent = "Like"),
+            500
+        );
+    }
 }
